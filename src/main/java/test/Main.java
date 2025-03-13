ea7336ff855dafeaa4b1b575a1c51def05a2f502
Spyridon Metaxiniadis <metaxins@gmail.com>
2025-03-13 12:18:18 +0200

    Added the first class "Name"

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("this is a test");
+        Names spyros = new Names();
+        System.out.println(spyros.toString());
     }
 }
---
+++ src/main/java/test/Names.java
@@ -0,0 +1,33 @@
+package test;
+
+public class Names {
+    private String name;
+    private String surname;
+
+    public Names() {
+        this.name = "N/A";
+        this.surname = "N/A";
+    }
+    public Names(String name){
+        this.name = name;
+        this.surname = "N/A";
+    }
+
+    public String getName (){
+        return this.name;
+    }
+    public String getSurname(){
+        return this.surname;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+    
+    public String toString (){
+        return this.name + " " + this.surname;
+    }
+}
